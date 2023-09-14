c8801775ce9caee34fee5e3aa049e15f285655aa
Ryan Volz <ryan.volz@gmail.com>
2023-09-14 01:31:24 -0400

    fixup! Initial feedstock commit with conda-smithy 3.25.1.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,6 @@
 setlocal EnableDelayedExpansion
 @echo on
 
-curl -o sdrplay_api.exe "https://www.sdrplay.com/software/SDRplay_RSP_API-Windows-%sdrplay_api_version%.exe"
-if errorlevel 1 exit 1
-sdrplay_api.exe /sp- /verysilent /suppressmsgboxes /norestart /currentuser /log="sdrplay_api_log.txt" /dir="%SRC_DIR%/sdrplay_api"
-
 :: Make a build folder and change to it
 mkdir build
 if errorlevel 1 exit 1
@@ -17,8 +13,8 @@ cmake -G "Ninja" ^
     -DCMAKE_INSTALL_LIBDIR:STRING=lib ^
     -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-    -DLIBSDRPLAY_INCLUDE_DIRS="%SRC_DIR%/sdrplay_api/API/inc" ^
-    -DLIBSDRPLAY_LIBRARIES="%SRC_DIR%/sdrplay_api/API/x64/sdrplay_api.lib" ^
+    -DLIBSDRPLAY_INCLUDE_DIRS="%SDRPLAY_PREFIX%\include" ^
+    -DLIBSDRPLAY_LIBRARIES="%SDRPLAY_PREFIX%\lib\sdrplay_api.lib" ^
     ..
 if errorlevel 1 exit 1
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,44 +2,14 @@
 
 set -ex
 
-cmake_config_args=(
-    -DCMAKE_BUILD_TYPE=Release
-)
-
-mkdir sdrplay_api
-pushd sdrplay_api
-
-if [[ $target_platform == linux-64 ]] ; then
-    wget "https://www.sdrplay.com/software/SDRplay_RSP_API-Linux-${sdrplay_api_version}.run"
-    7z x "SDRplay_RSP_API-Linux-${sdrplay_api_version}.run"
-    7z x "SDRplay_RSP_API-Linux-${sdrplay_api_version}"
-    cmake_config_args+=(
-        -DLIBSDRPLAY_INCLUDE_DIRS="$SRC_DIR/sdrplay_api/inc"
-        -DLIBSDRPLAY_LIBRARIES="$SRC_DIR/sdrplay_api/$(ls x86_64/libsdrplay_api.so*)"
-    )
-elif [[ $target_platform == linux-aarch64 ]] ; then
-    wget "https://www.sdrplay.com/software/SDRplay_RSP_API-ARM64-${sdrplay_api_version}.run"
-    7z x "SDRplay_RSP_API-ARM64-${sdrplay_api_version}.run"
-    7z x "SDRplay_RSP_API-ARM64-${sdrplay_api_version}"
-    cmake_config_args+=(
-        -DLIBSDRPLAY_INCLUDE_DIRS="$SRC_DIR/sdrplay_api/inc"
-        -DLIBSDRPLAY_LIBRARIES="$SRC_DIR/sdrplay_api/$(ls aarch64/libsdrplay_api.so*)"
-    )
-elif [[ $target_platform == osx* ]] ; then
-    wget "https://www.sdrplay.com/software/SDRplayAPI-macos-installer-universal-${sdrplay_api_version}.pkg"
-#     sudo installer -verbose -pkg "SDRplayAPI-macos-installer-universal-${sdrplay_api_version}.pkg" -target /
-    7z x "SDRplayAPI-macos-installer-universal-${sdrplay_api_version}.pkg"
-    7z x "Payload~"
-    cmake_config_args+=(
-        -DLIBSDRPLAY_INCLUDE_DIRS="$SRC_DIR/sdrplay_api/Library/SDRplayAPI/${sdrplay_api_version}/inc"
-        -DLIBSDRPLAY_LIBRARIES="$SRC_DIR/sdrplay_api/$(ls Library/SDRplayAPI/${sdrplay_api_version}/lib/libsdrplay_api.so*)"
-    )
-fi
-
-popd
-
 mkdir build
 cd build
+
+cmake_config_args=(
+    -DCMAKE_BUILD_TYPE=Release
+    -DLIBSDRPLAY_INCLUDE_DIRS="$SDRPLAY_PREFIX/include"
+    -DLIBSDRPLAY_LIBRARIES="$SDRPLAY_PREFIX/lib/libsdrplay_api.so"
+)
 
 cmake ${CMAKE_ARGS} -G "Ninja" .. "${cmake_config_args[@]}"
 cmake --build . --config Release -- -j${CPU_COUNT}
